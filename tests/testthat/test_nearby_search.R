ff7314c5d1fa859558ebf0359add3e2e1589153e
FosterLockerbie <FosterLockerbie>
2025-03-10 16:27:49 -0700

    minor code changes to nearbysearch

--- tests/testthat/test_nearby_search.R
+++ tests/testthat/test_nearby_search.R
@@ -5,17 +5,17 @@ context("Testing nearby_search")
 api_key = Sys.getenv("api_key")
 
 test_that("Function returns a data frame from valid location name", {
-  result <- nearby_search(location_name = "Vancouver", category = "restaurants", api_key = api_key)
+  result <- nearby_search(location_name = "Vancouver", type = "restaurants")
   expect_is(result, "data.frame")
 })
 
 test_that("Function can handle invalid category name", {
-  result <- nearby_search(location_name = "Calgary", category = "bowling alley", api_key = api_key)
+  result <- nearby_search(location_name = "Calgary", type = "bowling alley")
   expect_equal(nrow(result), 1)
 })
 
 test_that("Function handles successful request to API", {
-  result <- nearby_search(location_name = "cartagena", category = "hotels", api_key = api_key)
+  result <- nearby_search(location_name = "cartagena", type = "hotels")
   
   # check if fields are returned
   expect_true(all(is.na(result$location_id) | !is.na(result$location_id)))
@@ -23,3 +23,7 @@ test_that("Function handles successful request to API", {
   expect_true(all(is.na(result$distance) | !is.na(result$distance)))
   expect_true(all(is.na(result$bearing) | !is.na(result$bearing)))
 })
+
+
+
+
