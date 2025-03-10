6e605a24c33ec9153d03c94c9f5198f0b746f574
Kiran John <john.kiran04@gmail.com>
2025-03-10 14:17:03 -0700

    first tests

---
+++ tests/testthat/test_get_location_info.R
@@ -0,0 +1,24 @@
+library(TripadvisoR)
+
+context("Testing get_location_info")
+
+api_key = Sys.getenv("api_key")
+
+test_that("Function returns a data frame from valid location id", {
+  result <- get_location_info(4065774)
+  expect_is(result, "data.frame")
+})
+
+test_that("Function can handle invalid location id", {
+  result <- get_location_info(1234567890123)
+  expect_equal(nrow(result), 1)
+})
+
+test_that("Function handles successful request to API", {
+  result <- get_location_info(4065774)
+  
+  # check if fields are returned
+  expect_true(all(is.na(result$Name) | !is.na(result$Name)))
+  expect_true(all(is.na(result$Description) | !is.na(result$Description)))
+  expect_true(all(is.na(result$Category) | !is.na(result$Category)))
+})
