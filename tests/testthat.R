f83198cb65b59cbd52c5005ae323b22534b9da6a
Kiran John <john.kiran04@gmail.com>
2025-03-17 23:24:43 -0700

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
-# make sure to be inside the main package directory to run this
+# make sure to be inside the main package directory to run this (only works locally right now)
 library(testthat)
 library(TripadvisoR)
-test_check("TripadvisoR")
+test_dir("./tests/testthat")
